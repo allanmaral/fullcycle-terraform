3ad7fbf8cbcd17ab9e60f6decd91e51d9bcdace6
Allan Amaral <allanmaralr@gmail.com>
2024-09-15 23:33:09 -0300

    add simple data source example

--- basics/local.tf
+++ basics/local.tf
@@ -7,11 +7,19 @@ resource "local_file" "example" {
   content  = var.content
 }
 
+data "local_file" "exemple_content" {
+  filename = "example.txt"
+}
+
+output "data_source_result" {
+  value = data.local_file.exemple_content.content
+}
+
 variable "content" {
   type    = string
   default = "Hello World"
 }
 
-output "file-id" {
+output "file_id" {
   value = resource.local_file.example.id
 }
