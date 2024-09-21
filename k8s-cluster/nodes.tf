1bc304349e7e2b6992e3644f956bb4b90e48b65d
Allan Amaral <allanmaralr@gmail.com>
2024-09-20 23:45:04 -0300

    add kubeconfig generation

--- k8s-cluster/nodes.tf
+++ k8s-cluster/nodes.tf
@@ -49,3 +49,20 @@ resource "aws_eks_node_group" "node-1" {
     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
   ]
 }
+
+resource "aws_eks_node_group" "node-2" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node-1"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = aws_subnet.subnets[*].id
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+  ]
+}
